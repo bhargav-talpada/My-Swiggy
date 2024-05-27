eb516fd6fee71bc2cc7a05393153dbb96b27e8f5
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-05-28 00:50:31 +0530

    changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -48,37 +48,40 @@ const Body = () => {
     return resturentList.length === 0 ? <Shimmer /> : (
       <div className="flex justify-center ">
         <div className="body w-9/12">
-            <div className="filter flex justify-between items-center">
-              <div className="search m-2 p-3">
-                <input type="text" data-testid="searchInput" className="searchinp p-2 border border-solid border-black " placeholder="Search..." value={searchText} onChange={(e)=>{setSearchText(e.target.value)}}/>
-                <button className="searchbtn border border-green-300 rounded-md px-5 py-2 bg-green-200 m-4 cursor-pointer" onClick={()=>{
-                  const searchRestro = resturentList.filter(res => res.info.name.toLowerCase().includes(searchText.toLowerCase()));
-                  setFilterdResturent(searchRestro)
-                }}>Search</button>
-              </div>
-
-              <div>
-                <label className="text-xl"> UserName :  </label>
-                <input data-testid="userNameInput" className="border border-black p-2" onChange={(e) => setUserName(e.target.value)} value={loggedInUser} />
-              </div>
-
-              <div>
-                <button className="filter-btn px-5 py-2 mr-3 border border-gray-200 rounded-md bg-gray-200 cursor-pointer" onClick={() => {
-                  const filterdList = resturentList.filter(res => res.info.avgRating > 4.3)
-                  setFilterdResturent(filterdList)
-                }} >Top Rated Resturents</button>
-              </div>
-
+          <div className="filter flex justify-between items-center">
+            <div className="search m-2 p-3">
+              <input type="text" data-testid="searchInput" className="searchinp p-2 border border-solid border-black " placeholder="Search..." value={searchText} onChange={(e)=>{setSearchText(e.target.value)}}/>
+              <button className="searchbtn border border-green-300 rounded-md px-5 py-2 bg-green-200 m-4 cursor-pointer" onClick={()=>{
+                const searchRestro = resturentList.filter(res => res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+                setFilterdResturent(searchRestro)
+              }}>Search</button>
             </div>
-            <div x-data="{ slide: 0 }" class="">
+
+            <div>
+              <label className="text-xl"> UserName :  </label>
+              <input data-testid="userNameInput" className="border border-black p-2" onChange={(e) => setUserName(e.target.value)} value={loggedInUser} />
+            </div>
+
+            <div>
+              <button className="filter-btn px-5 py-2 mr-3 border border-gray-200 rounded-md bg-gray-200 cursor-pointer" onClick={() => {
+                const filterdList = resturentList.filter(res => res.info.avgRating > 4.3)
+                setFilterdResturent(filterdList)
+              }} >Top Rated Resturents</button>
+            </div>
+
+          </div>
+
+          {/* What's on your mind */}
+            
+          <div x-data="{ slide: 0 }" class="">
               <div className="flex justify-between items-center">
                 <h1 className="text-3xl ml-4">{resRowHeader}</h1>
                 <div className="flex justify-center text-3xl mr-3">
-                  <BsArrowLeftCircleFill  className="text-gray-500 transition-all duration-700 hover:scale-125 "/>
-                  <BsArrowRightCircleFill className="ml-4 text-gray-500 transition-all duration-700 hover:scale-125 " />
+                  <BsArrowLeftCircleFill  className="text-gray-500 transition-all duration-700 hover:scale-125 cursor-pointer"/>
+                  <BsArrowRightCircleFill className="ml-4 text-gray-500 transition-all duration-700 hover:scale-125 cursor-pointer" />
                 </div>
               </div>
-              <div className="resturent-carts flex space-x-4 overflow-x-auto overflow-y-hidden">
+              <div className="resturent-carts flex space-x-4 overflow-x-auto overflow-y-hidden" style={{ scrollbarWidth: 'thin', scrollbarColor: 'transparent transparent' }}>
                 { 
                   filterdResturent.map((resturent) => 
                     <Link to={"/restaurents/" + resturent.info.id} key={resturent.info.id} >
@@ -90,6 +93,29 @@ const Body = () => {
                 }
               </div>
             </div>
+
+          {/* Top Chains Restaurent Cards */}
+
+          <div x-data="{ slide: 0 }" class="">
+            <div className="flex justify-between items-center">
+              <h1 className="text-3xl ml-4">{resRowHeader}</h1>
+              <div className="flex justify-center text-3xl mr-3">
+                <BsArrowLeftCircleFill  className="text-gray-500 transition-all duration-700 hover:scale-125 cursor-pointer"/>
+                <BsArrowRightCircleFill className="ml-4 text-gray-500 transition-all duration-700 hover:scale-125 cursor-pointer" />
+              </div>
+            </div>
+            <div className="resturent-carts flex space-x-4 overflow-x-auto overflow-y-hidden" style={{ scrollbarWidth: 'thin', scrollbarColor: 'transparent transparent' }}>
+              { 
+                filterdResturent.map((resturent) => 
+                  <Link to={"/restaurents/" + resturent.info.id} key={resturent.info.id} >
+                    {
+                      resturent.info.promoted ? <ResturentCartPromoted resData={resturent} /> : <ResturentCart resData={resturent}  />
+                    }
+                  </Link>
+                )
+              }
+            </div>
+          </div>
         </div>
       </div>
     )
