ab62f17d950425ba733bc8ffc631272e3292daf5
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-05-31 00:34:03 +0530

    icons

--- src/components/ResturentCart.js
+++ src/components/ResturentCart.js
@@ -11,7 +11,7 @@ const ResturentCart = (props) =>{
         <div data-testid="resCard" className=" res-carts mt-8 w-64 h-[415px]  rounded-md transition-all duration-300 hover:scale-95" >
             <img src={REST_IMG_URL+cloudinaryImageId} className=" cart-img rounded-xl w-full h-44" />
             <h3 className="font-bold mt-3 ml-3 text-2xl">{name}</h3>
-            <h4 className="flex items-center ml-3"><MdStars className="text-2xl text-green-600 mr-1" /> {avgRating} • {sla.deliveryTime} Minutes </h4>
+            <h4 className="flex items-center ml-3"><MdStars className="text-2xl text-green-600 mr-1" /> {avgRating} • {sla.slaString}  </h4>
             <h4 className="my-1 font-semibold ml-3">{costForTwo}/-</h4> {/* ₹ = ctrl + alt + 4 */}
             <h4 className="my-1 text-gray-500 font-semibold ml-3">{cuisines.join(", ")}</h4>
             <h4 className="my-1 text-gray-500 font-semibold ml-3">{areaName}</h4>
