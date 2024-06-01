f9b7808dfc5625c27bb4b1701175e08faa4e6fdf
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-06-02 02:52:09 +0530

    cuisined

--- src/components/OnlineFoodDeliveryCards.js
+++ src/components/OnlineFoodDeliveryCards.js
@@ -7,12 +7,14 @@ const OnlineFoodDeliveryCards = (props) => {
     const {resData} = props;
 
     const [truncatedResTitle, setTruncatedResTitle] = useState('');
+    const [truncatedResCuisines, setTruncatedResCuisines] = useState('');
 
     const {cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla, areaName} = resData?.info;
 
     useEffect(() => {
         setTruncatedResTitle(truncate(name, 25)); // Adjust the length as needed
-      }, [name]);
+        setTruncatedResCuisines(truncate(cuisines.join(", "), 30));
+      }, [name,cuisines]);
 
     const truncate = (input, length) => {
         if (input.length > length) {
@@ -26,10 +28,10 @@ const OnlineFoodDeliveryCards = (props) => {
         <div data-testid="resCard" className=" res-carts mt-8 mx-4 w-64 h-[415px]  rounded-md transition-all duration-300 hover:scale-95" >
             <img src={REST_IMG_URL+cloudinaryImageId} className=" cart-img rounded-xl w-full h-44" />
             <h3 className="font-bold mt-3 ml-3 text-xl">{ truncatedResTitle}</h3>
-            <h4 className="flex items-center ml-3"><TiStarFullOutline className="text-2xl text-green-600 mr-1" /> {avgRating} • {sla.slaString}  </h4>
-            <h4 className="my-1 font-semibold ml-3">{costForTwo}/-</h4> {/* ₹ = ctrl + alt + 4 */}
-            <h4 className="my-1 text-gray-500 font-semibold ml-3">{cuisines.join(", ")}</h4>
-            <h4 className="my-1 text-gray-500 font-semibold ml-3">{areaName}</h4>
+            <h4 className="flex items-center ml-3 text-lg"><TiStarFullOutline className="text-2xl text-green-600 mr-1" /> {avgRating} • {sla.slaString}  </h4>
+            <h4 className="my-1 font-semibold ml-3 text-lg">{costForTwo}/-</h4> {/* ₹ = ctrl + alt + 4 */}
+            <h4 className="my-1 text-gray-500 font-semibold ml-3 text-lg w-full">{truncatedResCuisines}</h4>
+            <h4 className="my-1 text-gray-500 font-semibold ml-3 text-lg">{areaName}</h4>
         </div>
     </div>
   )
