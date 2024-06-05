e137bbb8e62bff71d5226e4137e668d81244721f
Bhargav Talpada <119996397+Bhuro11@users.noreply.github.com>
2024-06-05 15:56:20 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,7 +9,6 @@ import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 import OnYourMindCardList from "./OnYourMindCardList";
 import OnlineFoodDeliveryCards from "./OnlineFoodDeliveryCards";
 
-off day
 
 const Body = () => {
 
