ac0d650ebff24e6d9e2a73dc8823678389391bed
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-07-22 11:50:35 +0530

    cart img | text

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import ItemList from "./ItemList";
 import { clearCart } from "../redux/cartSlice";
+import { CART_IMG_URL } from "../utils/constants";
 
 const Cart = () => {
 
@@ -14,10 +15,11 @@ const Cart = () => {
 
     return(
         <div className="text-center m-4 p-4 pt-28">
-            <h1 className="text-2xl">Cart</h1>
             <div className="w-6/12 m-auto ">
-                <button className="p-2 m-2 bg-black text-white rounded-lg text-xl" onClick={handleClearcart}>Clear Cart</button>
-                {cartItems.length === 0 && <h1 className="text-xl mt-10 text-red-500">Go & Do Some More Shopping...</h1>} 
+                {/* <button className="p-2 m-2 bg-black text-white rounded-lg text-xl" onClick={handleClearcart}>Clear Cart</button> */}
+                <img src={CART_IMG_URL} alt="Cart Img" />
+                {cartItems.length === 0 && <h1 className="text-xl mt-6 text-[#535665] font-semibold">Your cart is empty</h1>} 
+                {cartItems.length === 0 && <h1 className="text-sm mt-2 text-[#7e808c] font-normal">You can go to home page to view more restaurants</h1>} 
                 <ItemList items={cartItems} />
             </div>
         </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,3 +7,5 @@ export const LOGO_URL = "https://cdn.worldvectorlogo.com/logos/swiggy-1.svg";
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.2629975&lng=70.7862588&restaurantId=";
 
 export const ONYOURMIND_IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
+
+export const CART_IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/2xempty_cart_yfxml0";
